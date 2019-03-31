d392aebe65eada0f2890cf10f9db610fd7ca181c
Kenny Stephens <kennethcstephens@gmail.com>
2019-03-31 11:27:18 -0600

    testing

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 
+const forecastRouter = require('./routes/forecast');
+
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
+app.use('/', forecastRouter);
 
-const port = (process.env.PORT || 5000);
+const port = (process.env.PORT || 5500);
 
 app.listen(port, () => {
   console.log(`Server is running on Port: ${port}`);
-})
+});
---
+++ controllers/weatherRoute.controller.js
@@ -0,0 +1,10 @@
+const fetch = require('node-fetch');
+
+exports.getForecast = (req, res, next) => {
+  fetch('https://api.darksky.net/forecast/9a9a1e6cdaf634513a02835dae240b2e/37.8267,-122.4233')
+  .then(res => res.json())
+  .then(result => {
+    console.log(result);
+  })
+  .catch(err => console.log(err));
+}

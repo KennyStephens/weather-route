68e60558ceacd32872cda91faf52154db106482d
Kenny Stephens <34525009+KennyStephens@users.noreply.github.com>
2019-04-01 13:25:02 -0600

    messing with opencage geocoder api

--- controllers/weatherRoute.controller.js
+++ controllers/weatherRoute.controller.js
@@ -1,11 +1,29 @@
 const fetch = require('node-fetch');
 
+// exports.getForecast = (req, res, next) => {
+//   fetch('https://api.darksky.net/forecast/9a9a1e6cdaf634513a02835dae240b2e/37.8267,-122.4233')
+//   .then(res => res.json())
+//   .then(result => {
+//     console.log(result);
+//     res.send(result);
+//   })
+//   .catch(err => console.log(err));
+// }
+
 exports.getForecast = (req, res, next) => {
-  fetch('https://api.darksky.net/forecast/9a9a1e6cdaf634513a02835dae240b2e/37.8267,-122.4233')
+  fetch('https://api.opencagedata.com/geocode/v1/json?q=Boston&key=8fc46226f4374af39d7c94dd39337768')
   .then(res => res.json())
-  .then(result => {
-    console.log(result);
-    res.send(result);
-  })
+  .then(data => console.log(data.results[0]))
   .catch(err => console.log(err));
+
+
 }
+
+
+// fetch('https://api.darksky.net/forecast/9a9a1e6cdaf634513a02835dae240b2e/37.8267,-122.4233')
+// .then(res => res.json())
+// .then(result => {
+//   console.log(result);
+//   res.send(result);
+// })
+// .catch(err => console.log(err));
